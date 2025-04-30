5c1e28416ec183695fcf7bbb58600d8b248bbe94
lmspr2025 <el_bambino16@hotmail.com>
2025-04-29 20:34:57 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// Simple countdown placeholder
+document.addEventListener("DOMContentLoaded", () => {
+  const timer = document.getElementById("timer");
+  if (timer) timer.textContent = "Launching Soon!";
+});
